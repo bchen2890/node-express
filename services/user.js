1a2ce2b22b8cb1e17207b0ae81a8b8111c3c7cbf
bchen2890 <99276640+bchen2890@users.noreply.github.com>
2022-03-13 22:26:50 +0100

    setTimeout

--- services/user.js
+++ services/user.js
@@ -1,3 +1,6 @@
+const print = (message) => {
+    console.log(message)
+}
 
 var user = {
     register: (req, res) => {
@@ -8,6 +11,12 @@ var user = {
         res.send(registerInfo);
     },
     welcome: (req, res) => {
+        setTimeout(() => {
+            // runs after 2 seconds
+            console.log("Timeout")
+        }, 2000)
+        setTimeout(print, 2000, "Timeout with parameter")
+
         res.send("Welcome " + req.query.name);
     }
 };
