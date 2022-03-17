e5e6e264911d28c62449b5ddb59733c05d71c8e5
bchen2890 <99276640+bchen2890@users.noreply.github.com>
2022-03-17 23:51:46 +0100

    Buffer module

---
+++ core_examples/buffer.js
@@ -0,0 +1,25 @@
+//Initializes a empty buffer (with bytes of zero)
+const emptyBuffer = Buffer.alloc(16);
+console.log("emptyBuffer:",emptyBuffer);
+//Buffer with integers representing bytes
+const numberBuffer = Buffer.from([8, 2, 3, 7, 4, 9]);
+console.log("numberBuffer:",numberBuffer);
+//Buffer with binary encoding in UTF-8
+const stringBuffer = Buffer.from("Hello World", 'utf-8');
+console.log("stringBuffer:", stringBuffer);
+
+//Write data to buffer
+emptyBuffer.write("Hello", "utf-8");
+console.log("write to emptyBuffer:", emptyBuffer);
+//Insert data at a position
+emptyBuffer.write(" World", 5, "utf-8");
+console.log("insert into emptyBuffer:", emptyBuffer);
+
+//Change a byte
+emptyBuffer[12] = "!".charCodeAt();
+//Convert to string
+console.log("emptyBuffer to String:", emptyBuffer.toString('utf-8'));
+
+//Get number of bytes
+console.log("length of emptyBuffer:", emptyBuffer.length)
+console.log("byteLength of emptyBuffer:", Buffer.byteLength(emptyBuffer));
