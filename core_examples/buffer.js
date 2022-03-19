b9a707ad7d51324d9232c931289b6514ff50f86f
bchen2890 <99276640+bchen2890@users.noreply.github.com>
2022-03-19 18:53:02 +0100

    Update buffer.js

--- core_examples/buffer.js
+++ core_examples/buffer.js
@@ -1,33 +1,49 @@
 //Initializes a empty buffer (with bytes of zero)
-const emptyBuffer = Buffer.alloc(16);
-console.log("emptyBuffer:",emptyBuffer);
+const emptyBuf = Buffer.alloc(16);
+console.log('emptyBuf:',emptyBuf);
 //Buffer with integers representing bytes
-const numberBuffer = Buffer.from([8, 2, 3, 7, 4, 9]);
-console.log("numberBuffer:",numberBuffer);
+const numberBuf = Buffer.from([8, 2, 3, 7, 4, 9]);
+console.log('numberBuf:',numberBuf);
 //Buffer with binary encoding in UTF-8
-const stringBuffer = Buffer.from("Hello World", 'utf-8');
-console.log("stringBuffer:", stringBuffer);
+const stringBuf = Buffer.from('Hello World', 'utf-8');
+console.log('stringBuf:', stringBuf);
 
 //Write data to buffer
-emptyBuffer.write("Hello", "utf-8");
-console.log("write to emptyBuffer:", emptyBuffer);
+emptyBuf.write('Hello', 'utf-8');
+console.log('write to emptyBuf:', emptyBuf);
 //Insert data at a position
-emptyBuffer.write(" World", 5, "utf-8");
-console.log("insert into emptyBuffer:", emptyBuffer);
+emptyBuf.write(' World', 5, 'utf-8');
+console.log('insert into emptyBuf:', emptyBuf);
 
 //Change a byte
-emptyBuffer[12] = "!".charCodeAt();
+emptyBuf[12] = '!'.charCodeAt();
 //Convert to string
-console.log("emptyBuffer to String:", emptyBuffer.toString('utf-8'));
+console.log('emptyBuf to String:', emptyBuf.toString('utf-8'));
+//Convert to JSON
+console.log('concat buffers:', emptyBuf.toJSON());
 
 //Get number of bytes
-console.log("length of emptyBuffer:", emptyBuffer.length)
-console.log("byteLength of emptyBuffer:", Buffer.byteLength(emptyBuffer));
+console.log('length of emptyBuf:', emptyBuf.length)
+console.log('byteLength of emptyBuf:', Buffer.byteLength(emptyBuf));
 
-//Copy buffer
-var copyBuffer = Buffer.from('aaaabbbbaaaabbbb');
-emptyBuffer.copy(copyBuffer);
-console.log("copy:",copyBuffer.toString());
+//Copy emptyBuf to copyBuf
+var copyBuf = Buffer.from('aaaabbbbaaaabbbb');
+emptyBuf.copy(copyBuf);
+console.log('copy to copyBuf:', copyBuf.toString());
+var otherBuf = Buffer.from('aaaabbbb');
+//Copy from a position
+copyBuf.copy(otherBuf, 4);
+console.log('copy to otherBuf at position 4:', otherBuf.toString());
 
 //Compare buffers
-console.log("compare:",Buffer.compare(emptyBuffer, copyBuffer));
+console.log('compare emptyBuf and copyBuf:', Buffer.compare(emptyBuf, copyBuf));
+console.log('compare emptyBuf and otherBuf:', Buffer.compare(emptyBuf, otherBuf));
+
+//Concat buffer
+var arr = [emptyBuf, otherBuf];
+var concatBuf = Buffer.concat(arr);
+var concatBufWithLength = Buffer.concat(arr, 18);
+
+console.log('concat buffers:', concatBuf.toString());
+console.log('concat buffers with length:', concatBufWithLength.toString());
+
