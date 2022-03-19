278844046b33e07bf36b73eba158a91183b8231a
bchen2890 <99276640+bchen2890@users.noreply.github.com>
2022-03-19 19:31:56 +0100

    Cipher and Decipher example

---
+++ core_examples/crypto.js
@@ -0,0 +1,28 @@
+const crypto = require('crypto');
+
+const ENCRYPTION_KEY = 'aaaabbbbaaaabbbbaaaabbbbaaaabbbb'; // Must be 32 characters
+const IV_LENGTH = 16; // 16 for AES, this is always 16
+var iv = crypto.randomBytes(IV_LENGTH);
+
+function encrypt(text) {
+    let cipher = crypto.createCipheriv('aes-256-cbc', Buffer.from(ENCRYPTION_KEY), iv);
+    let encrypted = cipher.update(text);
+
+    encrypted = Buffer.concat([encrypted, cipher.final()]);
+
+    return encrypted.toString('hex');
+}
+
+function decrypt(text) {
+    let encryptedText = Buffer.from(text, 'hex');
+    let decipher = crypto.createDecipheriv('aes-256-cbc', Buffer.from(ENCRYPTION_KEY), iv);
+    let decrypted = decipher.update(encryptedText);
+
+    decrypted = Buffer.concat([decrypted, decipher.final()]);
+
+    return decrypted.toString();
+}
+
+var encrypted = encrypt(Buffer.from('Hello'))
+console.log('encrypted text:', encrypted);
+console.log('decrypted text:', decrypt(encrypted));
