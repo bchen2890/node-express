2ec23fb130f3a2e56b35ab71dcc81ecd3d6faae1
bchen2890 <99276640+bchen2890@users.noreply.github.com>
2022-03-25 23:17:39 +0100

    setInterval() and setTimeout()

--- core_examples/timers.js
+++ core_examples/timers.js
@@ -1,4 +1,4 @@
-//immediate are executed after I/O events callbacks
+// Immediate are executed after I/O events callbacks
 setImmediate(()=> {
     setImmediate(() => console.log('immediate',1));
     setImmediate(() => {
@@ -9,8 +9,41 @@ setImmediate(()=> {
 
 console.log('Message executed before immediate');
 
-//Repeat after every t ms
-setInterval(()=> {
-    return console.log('interval');
-}, 1000);
+// Cancel a immediate
+var immediate = setImmediate(function A() {
+    console.log('immediate2 not canceled');
+});
+console.log('immediate2 canceling...');
+clearImmediate(immediate,
+    console.log('immediate2 canceled')
+);
+
+var timeout = 2500;
+
+// Executed one after t ms 
+setTimeout(() => {
+    console.log('timeout1', timeout, 'ms');
+}, timeout);
+
+// Cancel a timeout
+var st = setTimeout(() => {
+    console.log('timeout2 not canceled', timeout, 'ms');
+}, timeout);
+
+clearTimeout(st, console.log('timeout2 canceled'));
+
+
+var interval = 1000;
+
+// Repeat after every t ms
+var si = setInterval(() => {
+    console.log('interval every', interval, 'ms');
+}, interval);
+
+// Cancel a interval
+setTimeout(() => {
+    console.log('interval canceled')
+    clearInterval(si);
+}, 5000);
+
 
